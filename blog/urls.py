307506031d97fef5c81fd90e37ce4d3580b6f94a
yamamu <ponzoo55@gmail.com>
2019-05-06 15:08:39 +0900

    changed the html file for the site

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+"""
+mysite/urls.py から'http://127.0.0.1:8000/'に来た(adminがない)リクエストは、
+include文より、ここを参照するようになっている
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
